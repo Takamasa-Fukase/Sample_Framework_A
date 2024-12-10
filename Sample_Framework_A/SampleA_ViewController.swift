e6fced730ee7fa283117833b18c31ccf7c22ddfe
Takamasa-Fukase <ultrafukase@gmail.com>
2024-12-10 23:23:10 +0900

    ざっくり実装

---
+++ Sample_Framework_A/SampleA_ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SampleA_ViewController.swift
+//  Sample_Framework_A
+//
+//  Created by ウルトラ深瀬 on 10/12/24.
+//
+
+import UIKit
+
+public final class SampleFrameworkA {
+    public init() {}
+    
+    public func presentSampleA_VC(on presentingVC: UIViewController) {
+        print("FW_A presentSampleA_VC")
+        let vc = UIStoryboard(name: "SampleA_ViewController", bundle: .main).instantiateInitialViewController() as! SampleA_ViewController
+        presentingVC.present(vc, animated: true)
+    }
+}
+
+final class SampleA_ViewController: UIViewController {
+
+    @IBOutlet weak var sampleFrameworkB_BaseView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
